8376b1edd08866001bc6c4790f3cc1af3a6b3520
George Moura <gwmoura@gmail.com>
2018-08-09 18:02:10 -0300

    added some adjusts tu set default parameters and change base url

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # indice-sefip
+
+#### Fonte dos dados
+
+https://webp.caixa.gov.br/empresa/EditalFGTS/003/001/fgepw001.asp
+
+Parametros utilizados:
+
+* Opção FGTS:	Optantes após 22/09/1971
+* Categoria: Trabalhador
+
+#### Exemplos
+
+`curl http://localhost:5000/mes_ini_compt/04/mes_fim_compt/04/ano_ini_compt/1975/ano_fim_compt/1975/dia_ini_pgto/10/mes_ini_pgto/08/ano_ini_pgto/2018/dia_fim_pgto/10/mes_fim_pgto/08/ano_fim_pgto/2018/tipo_iden_empresa/1/cod_fpas/000/cod_simples/1/opcao/19710923/cod_categoria/1/index_categoria/1/index_opcao/6/index_tipo_iden_empresa/0/index_cod_fpas/0/index_cod_simples/0`
+
+irá gerar essa url
+
+`https://webp.caixa.gov.br/Empresa/EditalFGTS/003/001/Fgepw001a.asp?MesICompt=04&MesFCompt=04&AnoICompt=1975&AnoFCompt=1975&DiaIPgto=10&MesIPgto=08&AnoIPgto=2018&DiaFPgto=10&MesFPgto=08&AnoFPgto=2018&TipIdenEmp=&CodFpas=&CodSimples=&Opcao=19710923&CodCategoria=1&ICategoria=1&IOpcao=6&ITipIdenEmp=0&ICodFpas=0&ICodSimples=0`
+
+json de retorno:
+
+```json
+{
+  "competencia": "1975-04",
+  "data_pagamento": "2018-08-10",
+  "fator": 0.067794218,
+  "text": "Para SUA EMPRESAwin1=selffunction imprime(){  win1=window.open(\"/empresa/editalfgts/001/imprimir/imprime.htm\",'imprime','toolbar=no,location=no,directories=no,scrollbars=yes,Height=220,Width=400,top=0,left=0,resizable=no')  if(win1 != self) \t{\t\twin1.focus();\t}}  Edital Eletrnico    :: Coeficientes para Recolhimento Mensal em Atraso     Por recolhimento mensal ao FGTS em atraso entende-se aquele devido em face do disposto no Art. 15 da Lei n 8.036/90 e a Contribuio Social instituda pelo Art. 2 da Lei Complementar n 110/01, quando efetuados aps o vencimento regular da competncia.  \u00a0  Parmetros Informados:  \u00a0  Dados da Competncia:   Ms/Ano: 04/1975\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0a\u00a0\u00a0\u00a004/1975   Data para Pagamento: 10/08/2018\u00a0\u00a0\u00a0a\u00a0\u00a0\u00a010/08/2018   \u00a0   Dados do Trabalhador:   Opo: Optantes aps 22/09/1971   Categoria: \t\t1 - Trabalhador\t\t          \u00a0   Alquotas:   FGTS: 8%   Contribuio Social: Parmetros indicam no incidncia dessa Contribuio  \u00a0  \t\tA tabela apresentada a seguir est em conformidade com os parmetros informados acima.\t\t        Data dePagamento Competncias   \t  De 03 / 1975A\u00a0\u00a0\u00a005 / 1975\t   \t  \u00a0\t   \t  \u00a0\t   \t  \u00a0\t   \t  \u00a0\t     10/08/2018 0,067794218 \u00a0 \u00a0 \u00a0 \u00a0      ",
+  "url": "https://webp.caixa.gov.br/Empresa/EditalFGTS/003/001/Fgepw001a.asp?MesICompt=04&MesFCompt=04&AnoICompt=1975&AnoFCompt=1975&DiaIPgto=10&MesIPgto=08&AnoIPgto=2018&DiaFPgto=10&MesFPgto=08&AnoFPgto=2018&TipIdenEmp=&CodFpas=&CodSimples=&Opcao=19710923&CodCategoria=1&ICategoria=1&IOpcao=6&ITipIdenEmp=0&ICodFpas=0&ICodSimples=0"
+}
+```
--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_content(url):
 
 def generate_url(mes_ini_compt, mes_fim_compt, ano_ini_compt, ano_fim_compt, dia_ini_pgto, mes_ini_pgto, ano_ini_pgto, dia_fim_pgto, mes_fim_pgto, ano_fim_pgto, tipo_iden_empresa, cod_fpas, cod_simples, opcao, cod_categoria, index_categoria, index_opcao, index_tipo_iden_empresa, index_cod_fpas, index_cod_simples):
 
-    url = 'https://webp.caixa.gov.br/Empresa/EditalFGTS/004/001/Fgepw001a.asp?'
+    url = 'https://webp.caixa.gov.br/Empresa/EditalFGTS/003/001/Fgepw001a.asp?'
     url += 'MesICompt=' + str(mes_ini_compt).zfill(2) + '&MesFCompt=' + str(mes_fim_compt).zfill(2)
     url += '&AnoICompt=' + str(ano_ini_compt) + '&AnoFCompt=' + str(ano_fim_compt)
     url += '&DiaIPgto=' + str(dia_ini_pgto).zfill(2) + '&MesIPgto=' + str(mes_ini_pgto).zfill(2) + '&AnoIPgto=' + str(ano_ini_pgto)
@@ -33,31 +33,32 @@ def generate_url(mes_ini_compt, mes_fim_compt, ano_ini_compt, ano_fim_compt, dia
 
 @app.route("/mes_ini_compt/<int:mes_ini_compt>/mes_fim_compt/<int:mes_fim_compt>/ano_ini_compt/<int:ano_ini_compt>/ano_fim_compt/<int:ano_fim_compt>/dia_ini_pgto/<int:dia_ini_pgto>/mes_ini_pgto/<int:mes_ini_pgto>/ano_ini_pgto/<int:ano_ini_pgto>/dia_fim_pgto/<int:dia_fim_pgto>/mes_fim_pgto/<int:mes_fim_pgto>/ano_fim_pgto/<int:ano_fim_pgto>/tipo_iden_empresa/<tipo_iden_empresa>/cod_fpas/<cod_fpas>/cod_simples/<cod_simples>/opcao/<opcao>/cod_categoria/<cod_categoria>/index_categoria/<index_categoria>/index_opcao/<index_opcao>/index_tipo_iden_empresa/<index_tipo_iden_empresa>/index_cod_fpas/<index_cod_fpas>/index_cod_simples/<index_cod_simples>")
 def main(mes_ini_compt, mes_fim_compt, ano_ini_compt, ano_fim_compt, dia_ini_pgto, mes_ini_pgto, ano_ini_pgto, dia_fim_pgto, mes_fim_pgto, ano_fim_pgto, tipo_iden_empresa, cod_fpas, cod_simples, opcao, cod_categoria, index_categoria, index_opcao, index_tipo_iden_empresa, index_cod_fpas, index_cod_simples):
+    tipo_iden_empresa = ''
+    cod_fpas = ''
+    cod_simples = ''
     url = generate_url(mes_ini_compt, mes_fim_compt, ano_ini_compt, ano_fim_compt, dia_ini_pgto, mes_ini_pgto, ano_ini_pgto, dia_fim_pgto, mes_fim_pgto, ano_fim_pgto, tipo_iden_empresa, cod_fpas, cod_simples, opcao, cod_categoria, index_categoria, index_opcao, index_tipo_iden_empresa, index_cod_fpas, index_cod_simples)
 
     r = get_content(url)
     soup = process_content(r)
-    spans = soup.find_all('span', {'class': 'txtcentral8'})
-    factor = 0
+    spans = soup.find_all('span', {'class': 'txtcentral'})
+    factor = 0.0
 
     for i, span in enumerate(spans):
         content_tag = span.get_text().strip().replace('.','').replace(',','.')
-        try:
-            factor = float(content_tag)
-        except:
-            print("{} is not number".format(content_tag))
+        if content_tag != '':
+            try:
+                factor = float(content_tag)
+            except:
+                print("{} is not number".format(content_tag))
 
     data = {
         'url': url,
         'text': soup.get_text(),
         'fator': factor,
-        'mes_competencia': "{}-{}".format(ano_fim_compt, str(mes_fim_compt).zfill(2)),
+        'competencia': "{}-{}".format(ano_fim_compt, str(mes_fim_compt).zfill(2)),
         'data_pagamento': "{}-{}-{}".format(ano_ini_pgto,str(mes_ini_pgto).zfill(2),str(dia_ini_pgto).zfill(2))
     }
     return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-   # Exemplo de url: http://0.0.0.0:5000/mes_ini_compt/04/mes_fim_compt/06/ano_ini_compt/2007/ano_fim_compt/2007/dia_ini_pgto/30/mes_ini_pgto/05/ano_ini_pgto/2017/dia_fim_pgto/30/mes_fim_pgto/05/ano_fim_pgto/2017/tipo_iden_empresa/1/cod_fpas/000/cod_simples/1/opcao/1967/cod_categoria/1/index_categoria/1/index_opcao/1/index_tipo_iden_empresa/1/index_cod_fpas/3/index_cod_simples/1
